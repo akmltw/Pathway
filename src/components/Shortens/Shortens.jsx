0d4877b961d343c050af37fb7207c9dc96384548
Akim <akm_ltw@mail.ru>
2024-07-22 20:09:26 +0300

    Refactoring Shortens section with animated link list

--- src/components/Shortens/Shortens.jsx
+++ src/components/Shortens/Shortens.jsx
@@ -1,8 +1,11 @@
+import { useSelector } from 'react-redux';
+import { selectLinks } from 'store/slice/linkSlice';
 import { Button } from 'components/Button';
+import { motion, AnimatePresence } from 'framer-motion';
 import classes from './Shortens.module.scss';
 
 const Shortens = () => {
-    const links = [];
+    const links = useSelector(selectLinks);
 
     if (!links?.length) return null;
 
@@ -10,18 +13,21 @@ const Shortens = () => {
       <section className={classes.Shortens}>
           <div className='container'>
               {links.map(item => (
-                  <div
-                      key={item.code}
-                      className={classes.item}
+                <AnimatePresence key={item.code}>
+                  <motion.div
+                    className={classes.item}
+                    initial={{opacity: 0, height: 0}}
+                    animate={{opacity: 0, height: 'auto'}}
                   >
-                      <span>{item.original_link}</span>
-                      <span>{item.full_short_link2}</span>
-                      <Button
-                          variant="square"
-                      >
-                          Copy
-                      </Button>
-                  </div>
+                    <span>{item.original_link}</span>
+                    <span>{item.full_short_link2}</span>
+                    <Button
+                      variant="square"
+                    >
+                      Copy
+                    </Button>
+                  </motion.div>
+                </AnimatePresence>
               ))}
           </div>
       </section>
